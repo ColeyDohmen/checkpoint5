d2c5c8e43cf826f0a4d80dd5776e141ded3a35a3
ColeyDohmen <coley.dawn12@gmail.com>
2021-03-15 09:57:02 -0600

    accountBlogPage

--- src/pages/AccountPage.vue
+++ src/pages/AccountPage.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="about text-center">
-    <h1>Welcome {{ account.name }}</h1>
-    <img class="rounded" :src="account.picture" alt="" />
-    <p>{{ account.email }}</p>
-  </div>
-  <div class="div">
-    <h4>Your Blogs:</h4>
+  <div class="container-fluid">
+    <div class="about text-center">
+      <h1>Welcome {{ account.name }}</h1>
+      <img class="rounded" :src="account.picture" alt="" />
+      <p>{{ account.email }}</p>
+    </div>
+    <div class="div">
+      <h4>Your Blogs:</h4>
+      <p>None, apparently</p>
+    </div>
   </div>
 </template>
 
